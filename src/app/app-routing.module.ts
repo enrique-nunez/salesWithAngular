98fed44ccc1c38c60abfe7901f8b70e2c7068f6d
Jose Enrique Nuñez Constantino <j.nunez.constantino@gmail.com>
2020-09-18 14:29:22 -0500

    config router in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { ContactsComponent } from './page/contacts/contacts.component';
+import { HomeComponent } from './page/home/home.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  {path: '', component : MainComponent},
+const routes: Routes = [{
+  path: '', component : MainComponent,
+  children: [
+    {path: '', component: HomeComponent},
+    {path: 'contacts', component: ContactsComponent},
+    {path: '**', component: PageNotFoundComponent}
+  ]
+},
   {path: 'login', component : LoginComponent}
 ];
 
